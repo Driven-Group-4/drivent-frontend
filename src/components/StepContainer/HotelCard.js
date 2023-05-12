fadbd3941bd0e6e964164e52740ccad8623c2e85
Mikael Josias <mikaeljosias2002@gmail.com>
2023-05-11 22:36:06 -0300

    feat: hotels page

---
+++ src/components/StepContainer/HotelCard.js
@@ -0,0 +1,101 @@
+import styled from 'styled-components';
+
+export default function HotelCard({ hotelInfo, selectedCard, setSelectedCard }) {
+  const hotelCapacity = hotelInfo?.Rooms.reduce((acc, cv) => acc + cv.capacity - cv._count.Booking, 0);
+
+  function calculateHotelAccommodationType() {
+    let accomodationType = 'Não há acomodações';
+
+    const capacities = [];
+    hotelInfo?.Rooms.forEach((room) => {
+      if (!capacities.includes(room.capacity)) capacities.push(room.capacity);
+    });
+    capacities.sort((a, b) => a - b);
+
+    for (let i = 0; i < capacities.length; i++) {
+      if (i === 0) {
+        if (capacities[i] === 1) accomodationType = 'Single';
+        if (capacities[i] === 2) accomodationType = 'Duple';
+        if (capacities[i] === 3) accomodationType = 'Triple';
+        if (capacities[i] > 3) {
+          accomodationType = 'Outros';
+          break;
+        }
+        if (capacities.length === 2 || capacities[i + 1] > 3) accomodationType += ' e ';
+        if (capacities.length >= 3 && capacities[i + 1] <= 3) accomodationType += ', ';
+      }
+
+      if (i === 1) {
+        if (capacities[i] === 2) accomodationType += 'Duple';
+        if (capacities[i] === 3) accomodationType += 'Triple';
+        if (capacities[i] > 3) {
+          accomodationType += 'Outros';
+          break;
+        }
+        if (capacities.length > 2 && capacities[i] < 4) accomodationType += ' e ';
+      }
+
+      if (i === 2) {
+        if (capacities[i] === 3) accomodationType += 'Triple';
+        if(capacities[i] > 3) accomodationType += 'Outros';
+      }
+    }
+
+    return accomodationType;
+  }
+
+  return (
+    <StyledCard onClick={() => setSelectedCard(hotelInfo)} isSelected={selectedCard?.id === hotelInfo.id}>
+      <img src={hotelInfo?.image} alt={`Foto do hotel ${hotelInfo?.name}`} />
+      <CardTitle>{hotelInfo?.name}</CardTitle>
+      <div>
+        <CardSubtitle>Tipos de acomodação:</CardSubtitle><br/>
+        <CardText>{calculateHotelAccommodationType()}</CardText>
+      </div>
+      <div>
+        <CardSubtitle>Vagas disponíveis:</CardSubtitle><br/>
+        <CardText>{hotelCapacity}</CardText>
+      </div>
+    </StyledCard>
+  );
+}
+
+const StyledCard = styled.div`
+  display: flex;
+  flex-direction: column;
+  flex-shrink: 0;
+  gap: 12px;
+  background-color: ${props => props.isSelected ? '#FFEED2' : '#EBEBEB'};
+  border-radius: 10px;
+  padding: 16px 14px;
+  width: 196px;
+
+  & img {
+    border-radius: 5px;
+    width: 100%;
+    height: 110px;
+    object-fit: cover;
+  }
+
+  :hover{
+    cursor: pointer;
+  }
+`;
+
+const CardTitle = styled.span`
+  font-size: 20px;
+  font-weight: 400;
+  color: #343434;
+`;
+
+const CardSubtitle = styled.span`
+  font-size: 12px;
+  font-weight: 700;
+  color: #343434;
+`;
+
+const CardText = styled.span`
+  font-size: 12px;
+  font-weight: 400;
+  color: #343434;
+`;
--- src/pages/Dashboard/Hotel/index.js
+++ src/pages/Dashboard/Hotel/index.js
@@ -1,3 +1,29 @@
+import { Typography } from '@material-ui/core';
+import styled from 'styled-components';
+import StepContainer from '../../../components/StepContainer/StepContainer';
+import StepTitle from '../../../components/StepContainer/StepTitle';
+import useHotels from '../../../hooks/api/useHotel.js';
+import HotelCard from '../../../components/StepContainer/HotelCard';
+import OptionsContainer from '../../../components/StepContainer/OptionsContainer';
+import { useState } from 'react';
+
 export default function Hotel() {
-  return 'Hotel: Em breve!';
+  const [ selectedHotel, setSelectedHotel ] = useState(null);
+  const { hotels } = useHotels();
+
+  return (
+    <>
+      <StyledTypography variant="h4">Escolha de hotel e quarto</StyledTypography>
+      <StepContainer>
+        <StepTitle>Primeiro, escolha seu hotel</StepTitle>
+        <OptionsContainer>
+          { hotels?.map((h) => <HotelCard key={h.id} hotelInfo={h} selectedCard={selectedHotel} setSelectedCard={setSelectedHotel} />) }
+        </OptionsContainer>
+      </StepContainer>
+    </>
+  );
 }
+
+const StyledTypography = styled(Typography)`
+  margin-bottom: 20px!important;
+`;
