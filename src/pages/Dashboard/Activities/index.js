529c2f8c8b9646dac71a6a0cae438038894a9bd8
Mikael Josias <mikaeljosias2002@gmail.com>
2023-05-24 12:34:13 -0300

    feat: show event days

--- src/pages/Dashboard/Activities/index.js
+++ src/pages/Dashboard/Activities/index.js
@@ -1,3 +1,36 @@
+import { useContext, useState } from 'react';
+import EventInfoContext from '../../../contexts/EventInfoContext.js';
+import StepContainer from '../../../components/StepContainer/StepContainer.js';
+import StepTitle from '../../../components/StepContainer/StepTitle.js';
+import { Typography } from '@material-ui/core';
+import OptionsContainer from '../../../components/StepContainer/OptionsContainer.js';
+import DayCard from '../../../components/StepContainer/DayCard.js';
+import styled from 'styled-components';
+
 export default function Activities() {
-  return 'Atividades: Em breve!';
+  const [ selectedDate, setSelectedDate ] = useState(null);
+  const { eventInfo } = useContext(EventInfoContext);
+
+  const eventDays = [];
+  let currentDay = new Date(eventInfo?.startsAt);
+  while(currentDay <= new Date(eventInfo?.endsAt)) {
+    eventDays.push(new Date(currentDay));
+    currentDay.setDate(currentDay.getDate() + 1);
+  }
+
+  return (
+    <>
+      <StyledTypography variant="h4">Escolha de hotel e quarto</StyledTypography>
+      <StepContainer>
+        <StepTitle>Primeiro, filtre pelo dia do evento: </StepTitle>
+        <OptionsContainer>
+          { eventDays.map((day, i) => <DayCard key={i} date={day} selectedDate={selectedDate} setSelectedDate={setSelectedDate} />) }
+        </OptionsContainer>
+      </StepContainer>
+    </>
+  );
 }
+
+const StyledTypography = styled(Typography)`
+  margin-bottom: 20px!important;
+`;
